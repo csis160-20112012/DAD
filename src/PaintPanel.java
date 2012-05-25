e4635ae2825b2a481dcc30fb9a6b84a748215784
amela.hajdarevic <amela.hajdarevic@B113.ssst.local>
2012-05-25 15:04:33 +0200

    comm. to paiint pannel

--- src/PaintPanel.java
+++ src/PaintPanel.java
@@ -3,42 +3,38 @@ import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import javax.swing.JPanel;
+//starting program 
 
-public class PaintPanel extends JPanel 
-{
+public class PaintPanel extends JPanel {
 	private int pointCount = 0; // count number of points
 
 	// array of 10000 java.awt.Point references
-	private Point points[] = new Point[ 10000 ];  
+	private Point points[] = new Point[10000];
 
 	// set up GUI and register mouse event handler
-	public PaintPanel()
-	{
+	public PaintPanel() {
 		super();
-		
+
 		// handle frame mouse motion event
-		addMouseMotionListener( new PaintMouseListener() );
+		addMouseMotionListener(new PaintMouseListener());
 	}
 
 	// draw oval in a 5-by-5 bounding box at specified location on window
-	public void paintComponent( Graphics g )
-	{
-		super.paintComponent( g ); // clears drawing area
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g); // clears drawing area
 
 		// draw all points in array
-		for ( int i = 0; i < pointCount; i++ )
-			g.fillOval( points[ i ].x, points[ i ].y, 5, 5 );
+		for (int i = 0; i < pointCount; i++)
+			g.fillOval(points[i].x, points[i].y, 5, 5);
 	}
-	
+
 	// inner class PaintMouseListener has access to the paint points array
 	class PaintMouseListener extends MouseMotionAdapter {
-		
+
 		// store drag coordinates and repaint
-		public void mouseDragged( MouseEvent event )
-		{
-			if ( pointCount < points.length ) 
-			{
-				points[ pointCount ] = event.getPoint(); // find point
+		public void mouseDragged(MouseEvent event) {
+			if (pointCount < points.length) {
+				points[pointCount] = event.getPoint(); // find point
 				pointCount++; // increment number of points in array
 				repaint(); // repaint JFrame
 			}
--- src/Test.java
+++ src/Test.java
@@ -1,32 +1,25 @@
 
-
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 
-public class Test  {
+public class Test {
 
-	public static void main(String[] args){
-			BoxOfColors app=new BoxOfColors();
-			
+	public static void main(String[] args) {
+		BoxOfColors app = new BoxOfColors();
 
-			PaintPanel paintPanel = new PaintPanel(); // create paint panel
-			app.add( paintPanel, BorderLayout.CENTER ); // in center
+		PaintPanel paintPanel = new PaintPanel(); // create paint panel
+		app.add(paintPanel, BorderLayout.CENTER); // in center
 
-			// create a label and place it in SOUTH of BorderLayout
-			app.add( new JLabel( "Drag the mouse to draw" ), BorderLayout.SOUTH );
+		// create a label and place it in SOUTH of BorderLayout
+		app.add(new JLabel("Drag the mouse to draw"), BorderLayout.SOUTH);
 
-			app.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-			app.setSize( 400, 200 ); // set frame size
-			app.setVisible( true ); // display frame
-			app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			app.setDefaultCloseOperation(BoxOfColors.EXIT_ON_CLOSE);
-			
-		}
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		app.setSize(400, 200); // set frame size
+		app.setVisible(true); // display frame
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		app.setDefaultCloseOperation(BoxOfColors.EXIT_ON_CLOSE);
 
 	}
 
-           
-           
-
-	
+}
