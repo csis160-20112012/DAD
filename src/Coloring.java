bfb08bf046d6c3a6a1efc325529446db2ee82afb
amela.hajdarevic <amela.hajdarevic@B113.ssst.local>
2012-05-25 14:39:00 +0200

    Starting point of the project

---
+++ src/Coloring.java
@@ -0,0 +1,33 @@
+
+
+
+import java.awt.Graphics;
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class Coloring extends JPanel {
+	
+	public void paintComponent(Graphics g){
+		super.paintComponents(g);
+		this.setBackground(Color.WHITE);
+		
+		g.setColor(new Color (255,0,0));
+		g.fillRect(15,25,100,20);
+		g.drawString("Current RGB"+g.getColor(),130,40);
+		
+		
+		g.setColor(Color.BLUE);
+		g.fillRect(15, 50, 100, 20);
+		g.drawString("Current RGB"+g.getColor(), 130, 65);
+		
+		Color color=Color.MAGENTA;
+		g.setColor(color);
+		g.fillRect(15, 100, 100, 20);
+		g.drawString("RGB values"+color.getRed()+","+color.getGreen()+","+color.getBlue(),130,115);
+		
+		
+		
+	
+	}
+ 
+}
---
+++ src/Test.java
@@ -0,0 +1,32 @@
+
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+
+public class Test  {
+
+	public static void main(String[] args){
+			BoxOfColors app=new BoxOfColors();
+			
+
+			PaintPanel paintPanel = new PaintPanel(); // create paint panel
+			app.add( paintPanel, BorderLayout.CENTER ); // in center
+
+			// create a label and place it in SOUTH of BorderLayout
+			app.add( new JLabel( "Drag the mouse to draw" ), BorderLayout.SOUTH );
+
+			app.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+			app.setSize( 400, 200 ); // set frame size
+			app.setVisible( true ); // display frame
+			app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			app.setDefaultCloseOperation(BoxOfColors.EXIT_ON_CLOSE);
+			
+		}
+
+	}
+
+           
+           
+
+	
